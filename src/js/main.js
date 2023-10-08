6c578f17930fe24126058d6daf2a7928822dfd86
Kevin <kevinsilvarj10@gmail.com>
2023-10-08 18:19:54 -0300

    Base do código de trocar imagens do botão feito

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
+// *Hero section for shoes
+const options = document.querySelectorAll('.option');
+const bigShoes = document.getElementById('big-shoes');
+const bigShoesOriginal = bigShoes.getAttribute('src');
+
+// *Hero section shoes options cards
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+const option3 = document.getElementById('option3');
+
+// *Images to put when calling the function
+const img1 = './assets/images/big-shoe1.png';
+const img2 = './assets/images/big-shoe2.png';
+const img3 = './assets/images/big-shoe3.png';
+
 // *Hamburger menu variables
 const hamburgerMenu = document.getElementById('menu');
 const navbarList = document.getElementById('mobile');
@@ -10,4 +25,29 @@ const menuOpen = './assets/icons/hamburger-open.svg';
 hamburgerMenu.addEventListener('click', () => {
     navbarList.classList.toggle('scale-x-0');
     let teste = hamburgerMenu.getAttribute('src') === menuClose ? hamburgerMenu.setAttribute('src', menuOpen) : hamburgerMenu.setAttribute('src', menuClose);
+})
+
+
+
+// *Change image when clicking the card
+function changeImg(img) {
+    for (let el of options) {
+        el.addEventListener('click', () => {
+            if (el.getAttribute('src') !== bigShoesOriginal) {
+                bigShoes.setAttribute('src', img);
+            } else {
+                bigShoesOriginal;
+            }
+        })
+    }
+}
+
+option1.addEventListener('click', () => {
+    changeImg(img1);
+})
+option2.addEventListener('click', () => {
+    changeImg(img2);
+})
+option3.addEventListener('click', () => {
+    changeImg(img3);
 })
